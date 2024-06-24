c14d325c060a7cabf5685878a04372b923bda440
Vishu2323 <vishu.20b0131026@abes.ac.in>
2024-06-24 15:04:38 +0530

    Error & Response Handling

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import connectDB from "./db/index.js"
 import 'dotenv/config'
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runnning at PORT:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err);
+})
